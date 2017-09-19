a5b45f8592f441373d51e412e70fa9288f062389
Sung <k458850@gmail.com>
2017-09-19 18:33:25 +0800

    modify exception

--- SkyTree/js/index.js
+++ SkyTree/js/index.js
@@ -2,7 +2,6 @@
 
 //http://api.openweathermap.org/img/w/.png
 $(function () {
-
     if (navigator.geolocation) {
 
         navigator.geolocation.getCurrentPosition(getInformation, errorCallback);
@@ -11,50 +10,49 @@ $(function () {
         var gpsData = { "coords": { "latitude": '24.1372291', "longitude": '120.6809453' } }
         getInformation(gpsData);
     }
+})
 
-    function errorCallback(error) {
-        var errorTypes = {
-            0: "不明原因錯誤",
-            1: "使用者拒絕提供位置資訊",
-            2: "無法取得位置資訊",
-            3: "位置查詢逾時"
-        };
-        var gpsData = { "coords": { "latitude": '24.1372291', "longitude": '120.6809453' } }
-        getInformation(gpsData);
-    }
+function errorCallback(error) {
+    var errorTypes = {
+        0: "不明原因錯誤",
+        1: "使用者拒絕提供位置資訊",
+        2: "無法取得位置資訊",
+        3: "位置查詢逾時"
+    };
+    var gpsData = { "coords": { "latitude": '24.1372291', "longitude": '120.6809453' } }
+    getInformation(gpsData);
+}
 
-    function getInformation(location) {
-        //console.log(location.coords.latitude);
-        //console.log(location.coords.longitude);
+function getInformation(location) {
+    //console.log(location.coords.latitude);
+    //console.log(location.coords.longitude);
 
-        ///*weather*/
-        $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&units=metric&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
-            console.dir(data);
-            weatherData = data;
-            console.log(weatherData.main.temp);
-            console.log(weatherData.weather[0].description);
-            console.log(weatherData.weather[0].icon);
+    ///*weather*/
+    $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&units=metric&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
+        console.dir(data);
+        weatherData = data;
+        console.log(weatherData.main.temp);
+        console.log(weatherData.weather[0].description);
+        console.log(weatherData.weather[0].icon);
 
-            $("#address").html("<h1>" + weatherData.name + "</h1>");
-            $("#iconImg").html("<img src='http://api.openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png' />");
-            $("#description").html("<h2>" + weatherData.weather[0].description + "</h2>");
-            $("#tempValue").html("<h2>" + weatherData.main.temp + " °C</h2>");
-        });
+        $("#address").html("<h1>" + weatherData.name + "</h1>");
+        $("#iconImg").html("<img src='http://api.openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png' />");
+        $("#description").html("<h2>" + weatherData.weather[0].description + "</h2>");
+        $("#tempValue").html("<h2>" + weatherData.main.temp + " °C</h2>");
+    });
 
-        /*UV*/
-        $.get("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
-            console.dir(data);
-            uvData = data;
-            console.log(uvData[0].value);
-            $("#uvValue").html("<h2>UV: " + uvData[0].value + "</h2>");
-        });
-    }
-    $('#btnCpfRestart').click(function () {
-        if (RGBStart == 0) setup();
+    /*UV*/
+    $.get("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + location.coords.latitude + "&lon=" + location.coords.longitude + "&lang=zh_tw&appid=4b57e1c4d2edc6f01c8cb793bdea71ea", function (data) {
+        console.dir(data);
+        uvData = data;
+        console.log(uvData[0].value);
+        $("#uvValue").html("<h2>UV: " + uvData[0].value + "</h2>");
     });
     setup();
-    
-})
+}
+$('#btnCpfRestart').click(function () {
+    if (RGBStart == 0) setup();
+});
 
 function setup() {
     if (cpf) {
